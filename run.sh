13d75b7729d439f4a4c70b23d65df392d1bb260e
bulania <164865740+bulaniamaeli@users.noreply.github.com>
2024-05-28 05:41:51 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-06316732-305b-4afe-8017-abac9ecaeaab/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-16789ac2-04ae-458c-973c-7511626bb229/)
 
 
 echo "$curl_output" >> Update.tsx
